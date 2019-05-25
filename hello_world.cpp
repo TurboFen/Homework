511d2c06c8f854b5612489e64b77ec0edbea8d82
TurboFen <TurboFenix@yandex.ru>
2019-05-25 23:39:50 +0300

    change for clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <cstdlib> 
-
 int main() 
 { 
-    cout << "Your name"; //Enter your name
+    cout << Your name; //Enter your name
 // create a string (container)
 string name;
 cin << name;//Enter your name
-    cout << "Hello, world! from " << name << endl;
+    cout << Hello, world! from  << name << endl;
     return 0; 
 }
