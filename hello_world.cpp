670c2441e645a26441598307ab117efbfb48dd5d
TurboFen <TurboFenix@yandex.ru>
2019-05-25 22:50:04 +0300

    pull request modify

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,9 +3,10 @@
 
 int main() 
 { 
-    cout << "Your name";
+    cout << "Your name"; //Enter your name
+// create a string (container)
 string name;
-cin << name;
+cin << name;//Enter your name
     cout << "Hello, world! from " << name << endl;
     return 0; 
 }
