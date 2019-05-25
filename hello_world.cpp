9fa2eba22353b1be87c670a79ee626fd25c351a2
TurboFen <TurboFenix@yandex.ru>
2019-05-25 21:40:09 +0300

    my modified program

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main() 
 { 
-    cout << "Hello, world!" << endl;
+    cout << "Your name";
+string name;
+cin << name;
+    cout << "Hello, world! from " << name << endl;
     return 0; 
 }
